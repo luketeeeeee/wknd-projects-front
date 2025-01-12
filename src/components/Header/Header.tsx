0735db23c8a104a95acd77183f24d688a67648af
Lucas Henrique <henrique.lucas521@gmail.com>
2025-01-11 23:59:51 -0300

    some styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,29 @@
-import { CodeXml } from "lucide-react"
-import { Link } from "react-router"
+import { CodeXml } from "lucide-react";
+import { Link } from "react-router";
 
-import styles from "./Header.module.css"
+import styles from "./Header.module.css";
 
 export default function Header() {
-	return (
-		<>
-			<div className={styles.header}>
-				<div className={styles.logo}>
-					<CodeXml className={styles.logoIcon}/>
-					<h1 className={styles.logoText}>Weekend Projects</h1>
-				</div>
+  return (
+    <>
+      <div className={styles.header}>
+        <div className={styles.logo}>
+          <CodeXml className={styles.logoIcon} />
+          <h1 className={styles.logoText}>Weekend Project</h1>
+        </div>
 
-				<div className={styles.headerInfoSection}>
-					<Link to="/como-funciona">Como funciona</Link>
-					<Link to="/projetos">Projetos</Link>
-					<Link to="/login">Começar agora</Link>
-				</div>
-			</div>
-		</>
-	)
+        <div className={styles.headerInfoSection}>
+          <Link to="/como-funciona" className={styles.link}>
+            Como funciona
+          </Link>
+          <Link to="/projetos" className={styles.link}>
+            Projetos
+          </Link>
+          <Link to="/login" className={styles.startButton}>
+            Começar agora
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
