6469b216e5e891ceceff5b47e7251da9e78ec26d
Lucas Henrique <henrique.lucas521@gmail.com>
2025-01-15 00:08:16 -0300

    fix home page code structure

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import { ArrowRight, Code2, Rocket, Users } from "lucide-react";
 import { Link } from "react-router";
 
+import { cardsArray } from "./cards";
 import styles from "./Home.module.css";
 
 export default function Home() {
@@ -29,47 +30,50 @@ export default function Home() {
             Explorar projetos
           </Link>
         </div>
+      </div>
 
-        <div className={styles.mainInfoSection}>
-          <h1>Como funciona</h1>
+      <div className={styles.mainInfoSection}>
+        <h1>Como funciona</h1>
 
-          <div className={styles.infoCards}>
-            <div className={styles.card}>
-              <div>
-                <Rocket />
-              </div>
-              <h2>Compartilhe sua ideia</h2>
-              <p>
-                Publique suas ideias com uma pequena descrição e habilidades que
-                você julga necessárias
-              </p>
+        <div className={styles.infoCards}>
+          <div className={styles.card}>
+            <div>
+              <Rocket />
             </div>
+            <h2>Compartilhe sua ideia</h2>
+            <p>
+              Publique suas ideias com uma pequena descrição e habilidades que
+              você julga necessárias
+            </p>
+          </div>
 
-            <div className={styles.card}>
-              <div>
-                <Users />
-              </div>
-              <h2>Monte sua equipe</h2>
-              <p>
-                Publique suas ideias com uma pequena descrição e habilidades que
-                você julga necessárias
-              </p>
+          <div className={styles.card}>
+            <div>
+              <Users />
             </div>
+            <h2>Monte sua equipe</h2>
+            <p>
+              Publique suas ideias com uma pequena descrição e habilidades que
+              você julga necessárias
+            </p>
+          </div>
 
-            <div className={styles.card}>
-              <div>
-                <Code2 />
-              </div>
-              <h2>Desenvolva</h2>
-              <p>
-                Ponha as suas ideias (ou atividades) em prática para
-                transformá-las em realidade
-              </p>
+          <div className={styles.card}>
+            <div>
+              <Code2 />
             </div>
+            <h2>Desenvolva</h2>
+            <p>
+              Ponha as suas ideias (ou atividades) em prática para
+              transformá-las em realidade
+            </p>
           </div>
         </div>
+      </div>
 
-        <div></div>
+      <div className={styles.projectCards}>
+        <h1>Veja alguns projetos</h1>
+        <div className={styles.projectsCarousel}></div>
       </div>
     </div>
   );
---
+++ src/pages/Home/cards.ts
@@ -0,0 +1,35 @@
+export const cardsArray: {
+  id: number;
+  projectName: string;
+  projectLookingFor: string;
+  projectDescription: string;
+}[] = [
+  {
+    id: 1,
+    projectName: "Projeto Emprego 2025",
+    projectLookingFor: "Frontend Veloz, Backend Cerebral",
+    projectDescription:
+      "Tenho que conseguir um emprego esse ano, vai Corinthians",
+  },
+  {
+    id: 2,
+    projectName: "Projeto Emprego 2025",
+    projectLookingFor: "Frontend Veloz, Backend Cerebral",
+    projectDescription:
+      "Tenho que conseguir um emprego esse ano, vai Corinthians",
+  },
+  {
+    id: 3,
+    projectName: "Projeto Emprego 2025",
+    projectLookingFor: "Frontend Veloz, Backend Cerebral",
+    projectDescription:
+      "Tenho que conseguir um emprego esse ano, vai Corinthians",
+  },
+  {
+    id: 4,
+    projectName: "Projeto Emprego 2025",
+    projectLookingFor: "Frontend Veloz, Backend Cerebral",
+    projectDescription:
+      "Tenho que conseguir um emprego esse ano, vai Corinthians",
+  },
+];
